d6092711e0008d2f64e935326ddaa77fe8f71a30
BoraALAP <alapbora@gmail.com>
2017-04-05 23:00:00 -0400

    landing page is almost done with the animations

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
 
 $( document ).ready(function() {
     $(".enterence").addClass("enterence_open");
+    $(".img_container").addClass("img_load_animation");
+
+    $(function() {
+    //caches a jQuery object containing the header element
+    var header = $(".enterence");
+    var img_container = $(".img_container");
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 100) {
+            header.removeClass('enterence_open').addClass("enterence_short");
+            img_container.addClass("enterence_short_img");
+        } else {
+            header.removeClass("enterence_short").addClass('enterence_open');
+            img_container.removeClass("enterence_short_img");
+        }
+    });
+});
 });
 
 
