031a248bd69efa3addd3676b17bfb061c53d3df4
BoraALAP <alapbora@gmail.com>
2017-04-07 01:48:22 -0400

    Contact page is almost done

--- js/main.js
+++ js/main.js
@@ -1,24 +1,50 @@
 
 $( document ).ready(function() {
-    $(".enterence").addClass("enterence_open");
-    $(".img_container").addClass("img_load_animation");
+	var header = $(".enterence");
+    var page_container = $(".page_container");
+///////// Page Load Animation
+    header.addClass("enterence_open");
+    page_container.addClass("img_load_animation");
+///////// Page Load Animation End
+    
 
+///////// Activating Animation on scroll
     $(function() {
     //caches a jQuery object containing the header element
-    var header = $(".enterence");
-    var img_container = $(".img_container");
+    
+   
     $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
-
-        if (scroll >= 100) {
+        var scroll = $(window).scrollLeft();
+        if (scroll >= 20) {
             header.removeClass('enterence_open').addClass("enterence_short");
-            img_container.addClass("enterence_short_img");
+            page_container.addClass("enterence_short_img");
         } else {
             header.removeClass("enterence_short").addClass('enterence_open');
-            img_container.removeClass("enterence_short_img");
+            page_container.removeClass("enterence_short_img");
         }
     });
+///////// Activating Animation on scroll End
+
+
+///////// Horizontal Scrolling	
+    $(function() {
+	   $("body").mousewheel(function(event, delta) {
+
+	      this.scrollLeft -= (delta * 5);
+	    
+	      event.preventDefault();
+
+	   });
+	});
+///////// Horizontal Scrolling End
+
+	$('.svg').click(function(){
+		
+		$('html, body').animate({scrollLeft : 400},2500);
+		return false;
+	});
 });
 });
+
 
 
